5ba290c7151c1eea38ae9624da3702308d6f7269
RevanoAugustofa <revanoa4945@gmail.com>
2025-03-06 15:01:34 +0700

    up

--- resources/views/Admin/dashboard_adm.blade.php
+++ resources/views/Admin/dashboard_adm.blade.php
@@ -10,7 +10,45 @@
 </head>
 <body>
     <x-layout_admin > 
-        
+      <div class="flex-1 p-6">
+        <div class="mt-4">
+            <p class="text-gray-600">> Data Mahasiswa</p>
+            <h2 class="text-2xl font-bold">Mahasiswa <span class="text-gray-600 text-sm">Riwayat berhenti studi mahasiswa.</span></h2>
+        </div>
+        <div class="bg-white p-4 rounded shadow mt-4">
+             <!-- Tab Menu -->
+            <div class="flex justify-between items-center border-b pb-2">
+                <div class="flex space-x-4">
+                    <button class="px-4 py-2 border-b-4 border-blue-500 font-semibold">Data Mahasiswa</button>
+                   <a href="pengajuanCuti"><button class="px-4 py-2 text-gray-600 hover:bg-gray-100 rounded ">Pengajuan Cuti</button></a> 
+                   <a href="riwayat"><button class="px-4 py-2  text-gray-600 hover:bg-gray-100 rounded  ">Timeline Pengajuan</button></a>
+                </div>
+            </div>
+            <div class="flex flex-col md:flex-row pt-4">
+                <div class="bg-gray-200 h-48 w-48 flex items-center justify-center mr-6 mb-4 md:mb-0">
+                    <span>Foto</span>
+                </div>
+                <div class="w-full bg-gray-50 p-4 rounded">
+                    <h3 class="font-bold">Data Mahasiswa</h3>
+                    <table class="w-full mt-2 border border-collapse table-auto">
+                        <tbody class="divide-y divide-gray-300">
+                            <tr class="bg-gray-100"><td class="p-4 font-semibold w-3/4">NPM</td><td class="p-4 w-2/3" id="npm"></td></tr>
+                            <tr class="bg-white"><td class="p-4 font-semibold">Nama</td><td class="p-4" id="nama">-</td></tr>
+                            <tr class="bg-gray-100"><td class="p-4 font-semibold">Tempat/ Tgl Lahir</td><td class="p-4" id="ttl"></td></tr>
+                            <tr class="bg-white"><td class="p-4 font-semibold">Jenis Kelamin</td><td class="p-4" id="jenis_kelamin"></td></tr>
+                            <tr class="bg-gray-100"><td class="p-4 font-semibold">Agama</td><td class="p-4" id="agama"></td></tr>
+                            <tr class="bg-white"><td class="p-4 font-semibold">Tahun Akademik</td><td class="p-4" id="tahun_akademik"></td></tr>
+                            <tr class="bg-gray-100"><td class="p-4 font-semibold">Program Studi</td><td class="p-4" id="prodi"></td></tr>
+                            <tr class="bg-white"><td class="p-4 font-semibold">Jurusan</td><td class="p-4" id="jurusan"></td></tr>
+                            <tr class="bg-white"><td class="p-4 font-semibold">Alamat</td><td class="p-4" id="alamat"></td></tr>
+                            <tr class="bg-gray-100"><td class="p-4 font-semibold">Email</td><td class="p-4" id="email"></td></tr>
+                            <tr class="bg-white"><td class="p-4 font-semibold">No Handphone</td><td class="p-4" id="no_hp"></td></tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
     </x-layout_admin > 
 </body>
 </html>
